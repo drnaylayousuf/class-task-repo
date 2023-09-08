7d58cc8cb6825c2a65635acb34c8b641eaf264ab
drnaylayousuf <naylayousuf.dr1310@gmail.com>
2023-09-08 14:11:35 +0500

    class task

---
+++ app.js
@@ -0,0 +1,343 @@
+//chapter no 1
+
+// Q1
+// alert("Welcome To My Page!")
+
+// Q2
+// alert('javascript alert\n Error ! Please enter a valid password!')
+
+// Q3
+// alert("Javascript Alert \n\n Welcome To JS Land....\nHappy Coding!");
+
+// Q4
+// alert("Welcome To Js Land...")
+// alert("Javascript Alert \n\nHappy Coding!\nprevent this page from creating additional dialog.")
+
+// Q5
+// alert("Javascript Alert!\n\n Hello... I can run JS through my web browser console.");
+
+// Q6  done
+
+// Q7  done
+
+
+// Chapter 2 
+// Q1
+// var username;
+
+
+// Q2
+// var myName = "nayla Yousuf";
+
+
+// Q3
+// a. 
+// var message;
+// b.
+// var message="Hello World";
+// c.
+// alert("message");
+
+
+// Q4
+// var firstname = "jhone ";
+// var lastname ="Doe";
+// var age = "15 year old";
+
+// var course = "Certified Mobile Application Development";
+// alert(firstname+" "+lastname);
+// alert(age);
+// alert(course);
+
+// Q5
+// var food ="Pizza\npizz\nplz\np";
+// alert(food);
+
+// Q6
+// var email = "example@example.com";
+// var email= "Naylayousuf@gmail.com";
+// alert("My email address is " + email);
+
+// Q7
+// var book = "“A Smarter way to learn JavaScript”";
+// alert("I am trying to learn from the book " + book);
+
+// Q8
+// document.write("Yah! I can write JS throught Javascript!");
+
+// Q9
+// var show = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(show);
+
+// chapter n0 3 
+// Q1
+// var age =  " i am 20 year old";
+// alert(age);
+
+    //   another way or second way
+// var age= 19;
+// alert("I am " + age + " Year Old ");
+
+
+// Q2
+// var visits=0;
+// visits++;
+// alert("You have visited this site " + visits +" times");
+
+// Q3
+// var birthYear = 2000;
+//  document.write("My birth year is " + birthYear + "<br> Data type of my declared variable is number");
+
+// Q4
+// var visitor_name="John Doe";
+//  var product_title="T-shirt(s)";
+// var quantity="5";
+
+// document.write("<br>" + visitor_name + " ordered " + quantity + " " + product_title + " on XYZ clothing store");
+
+// chapter no 4
+// Q1
+//  var num1,num2,num3;
+                                           
+//  another way  
+//  var num1;
+//  var num2;
+//  var num3;
+
+
+
+
+// Q2  
+//    5 legal variables
+
+// var naylaYousuf;
+// var nayla_Yousuf;
+// var naylayousuf;
+// var naylayousuf123;
+// var $naylayousuf;
+
+// 5 illegal variables
+
+// var 123naylaYousuf;
+// var nayla-Yousuf;
+// var nayla yousuf;
+// var nayla&yousuf123;
+// var 123;
+
+// Q3
+
+//  {document.write(" “Rules for naming JS variables”<br> <br><br><br> Variable names can only contain,numbers,$ and _. For example: $my_1st variable<br> Variable must begin with a letter, $ or _ . For example: $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords");
+
+//  }
+
+// chapter no 5 
+// Q1
+
+//  var number1 = 3;
+//  var number2 = 5;
+
+//  var sum = number1 + number2;
+// document.write("sum of 3 and 5 is "+sum)
+
+
+// Q2
+// part 1 subtraction
+// var number1 = 3;
+// var number2 = 5;
+// var sub=number1 - number2;
+//  document.write ("subtract of 3 and 5 is "+ sub); 
+
+// part2 
+// multiplication
+// var number1 = 3;
+// var number2 = 5;
+// document.write ("multiply of 3 and 5 is " + number1 * number2)
+
+
+// part3 
+// division
+//  var number1 = 3;
+//  var number2 = 5;
+//  document.write ("division of 3 and 5 is "+ number1 / number2)
+
+// part 4
+// modulus
+
+// var number1 = 3;
+//   var number2 = 5;
+//   document.write ("modulus of 3 and 5 is "+ number1 % number2)
+
+// Q3
+// a
+// var value;
+//   document.write("value after variaable declaration is  " + value)
+
+//  var value = 5;
+//  document.write("<br> Initial value:" + value)
+
+//  var value = 5;
+//  var result = value++;
+ 
+//  document.write("<br>value after increment is "+ value);
+
+//  var value2 = 7;
+//  var  result = value + value2;
+// //h   
+// document.write("<br>value after addition is " + result)
+
+// var value =13;
+// var result = value--;
+
+// document.write("<br>Value after decrement is "+ value)
+
+// var calculation = value % 3;
+// document.write("<br> The remainder is: "+ calculation)
+
+// Q4
+
+// var cost1 =600;
+// var cost2 =5;
+// var result= (cost1 * cost2)
+
+// document.write("Total cost to buy 5 tickets to a movie is  "+ result+ "PKR")
+
+// Q5
+
+// document.write("Table of 4")
+
+// var calculation = 4*1;
+// document.write("<br> 4 x 1 = "+ calculation)
+
+
+// var calculation = 4*2;
+// document.write("<br> 4 x 2 = "+ calculation)
+
+
+// var calculation = 4*3;
+// document.write("<br> 4 x 3 = "+ calculation)
+
+// var calculation = 4*4;
+// document.write("<br> 4 x 4 = "+ calculation)
+
+// var calculation = 4*5;
+// document.write("<br> 4 x 5 = "+ calculation)
+
+// var calculation = 4*6;
+// document.write("<br> 4 x 6 = "+ calculation)
+
+// var calculation = 4*7;
+// document.write("<br> 4 x 7 = "+ calculation)
+
+// var calculation = 4*8;
+// document.write("<br> 4 x 8 = "+ calculation)
+
+// var calculation = 4*9;
+// document.write("<br> 4 x 9 = "+ calculation)
+
+// var calculation = 4*10;
+// document.write("<br> 4 x 10 = "+ calculation)
+
+
+// Q6
+
+
+// celsius to farenheit (celsius)
+
+// a
+//  var celsius = 25
+//  b
+// var  celsiusInf = (celsius*9)/5 +32;
+// document.write(celsius + "°C is "+ celsiusInf  + "°F");
+
+// c
+// var farenheit = 70; 
+//  d
+// var  farenheitInc = (farenheit - 32)*5/9;
+// document.write("<br>"+farenheit + "°F is "+ farenheitInc  + "°C");
+
+// Q7
+
+// document.write("SHOPPING CART");
+// var priceofItem1 = 650;
+// var  quantityofItem1 = 3;
+// var totalChargeofItem1 = (650 * 3)
+
+// var priceofItem2 = 100;
+// var  quantityofItem2 = 7;
+// var totalChargeofItem2 = (100 * 7);
+
+// var totalCharges = (totalChargeofItem1 + totalChargeofItem2);
+
+// var shippingCharges = 100;
+// var totalCost = (totalCharges + shippingCharges);
+// document.write("<br><br> Price of Item 1 is: " + priceofItem1);
+// document.write("<br> Order Quantity of Item 1 is: " + quantityofItem1);
+// document.write("<br> Price of Item  2 is: " + priceofItem2);
+// document.write("<br> Order Quantity of Item 2 is: " + quantityofItem2);
+// document.write("<br> Shipping charges is " + shippingCharges);
+// document.write("<br><br>Total Cost of your order is: " + totalCost);
+
+
+// Q8
+
+// var totalMarks = 980;
+// document.write("Total Marks: " + totalMarks)
+//  var  marksObtained = 804;
+//  document.write("<br>Marks Obtained: " + marksObtained)
+//  var calculate = + marksObtained / totalMarks* 100
+ 
+//   document.write("<br>percentage:" + calculate + " %")
+
+// Q9
+
+// var priceof10Dollar =1048;
+// var priceof25Saudiriyal =700;
+
+// var calculate = priceof10Dollar + priceof25Saudiriyal;
+// document.write("Total Currency in PKR: " + calculate)
+
+// Q10 
+
+//   var initialize = 5;
+//   var calculate = (initialize + 5 * 10 / 2);
+//    console.log(calculate);
+
+// Q11
+
+//  document.write("AGE CALCULATOR <br><br>")
+ 
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var age = currentYear - birthYear;
+// var result =age;
+// document.write("Your Age is: " +  age + " (age -1) then  it will be then 23 depending on which month it will be in 2016.");
+
+
+// Q12
+
+// var radius = 20;
+// document.write("Radius of a Circle : " + radius );
+// var circumference = Math.PI * 2 * radius;
+// document.write("<br>The circumference is: " + circumference);
+// var area = Math.PI * radius*radius;
+// document.write("<br> The area is: " + area);
+
+
+
+
+
+// Q13
+
+// document.write("The Lifetime Supply Calculator<br><br>")
+
+// document.write("Favourite Snacks: Chocolate chips <br>");
+// var age =   15;
+// var maxAge = 65;
+//  var perDay = 3;
+//  document.write("Current age: " + age + "<br>");
+//  document.write("Estimated Maximun Age: " + maxAge +
+//  "<br>");
+//  document.write("Amount of snacks per day: " + perDay + "<br>");
+//  var totalNeeded =(maxAge - age) * 3 ;
+//  var message = totalNeeded;
+//  document.write("You will need " + message + " Chocolate chip to last until the ripe old age of 65");
